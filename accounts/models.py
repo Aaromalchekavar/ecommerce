4d0fb9254c38d987d17a6f7d97f97c17e00c3ea7
Aaromalchekavar <binilbijuchengankal@gmail.com>
2021-10-27 00:49:09 +0530

    price/pmethod fix

--- accounts/models.py
+++ accounts/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='productImages',null=False, blank=False)
     name = models.CharField(max_length=200, null=False, blank=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=False, null=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(decimal_places=2)
     description = models.TextField()
     quantity = models.IntegerField()
     is_published = models.BooleanField(default=True)
